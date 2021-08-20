56a6d3ad32d132aa4105c88fa8888753b4749584
mcqueen7 <83740852+mcqueen7@users.noreply.github.com>
2021-08-20 11:42:40 +0800

    Back to High School Physics

---
+++ Back to High School Physics.cpp
@@ -0,0 +1,14 @@
+#define BacktoHighSchoolPhysicsOK
+#ifdef BacktoHighSchoolPhysics
+#include<iostream>
+using namespace std;
+int main() {
+	int v, t;
+	while (cin>>v>>t)
+	{
+		cout << v * t * 2 << endl;
+	}
+
+	system("pause"); return(0);
+}
+#endif
